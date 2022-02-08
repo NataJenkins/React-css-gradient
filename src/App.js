6c3b46b47174e345546865e63f5c07e4b2882291
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2022-02-07 23:31:24 -0600

    content component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Content from "./components/content/Content";
 import Sidebar from "./components/sidebar/Sidebar.jsx";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Content />
     </div>
   );
 }
---
+++ src/components/content/Content.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./content.css";
+
+export default function Content() {
+  return (
+    <div className="content">
+      <div className="gradient"></div>
+      <div className="info">
+        <section className="about">
+          <h2>About CSS Gradients</h2>
+          <p>
+            The CSS Gradient online generator tool is a nice and simple to use
+            utility to quickly generate linear and radial color gradients. You
+            can create the gradients and export the CSS code with colors in HEX
+            or RGB format.
+          </p>
+          <p>
+            Keep reading below to learn more about <span>Linear Gradients</span>
+            , <span>Radial Gradients</span> or <span>Text Gradients</span>.
+          </p>
+          <p>
+            Or, get inspired and discover more gradient backgrounds by color:{" "}
+            <span>Red Gradient backgrounds</span>{" "}
+            <span>Orange Gradient backgrounds</span>{" "}
+            <span>Yellow Gradient backgrounds</span>{" "}
+            <span>Green Gradient backgrounds</span>
+            <span>Azure Gradient backgrounds</span>
+            <span>Blue Gradient backgrounds</span>
+            <span>Purple Gradient backgrounds</span>
+            <span>Pink Gradient backgrounds</span> or
+            <span>Monochrome Gradient backgrounds</span>
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+}
