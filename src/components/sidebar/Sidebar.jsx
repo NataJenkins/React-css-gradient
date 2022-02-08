153b54cfe6054ab661f1eb0a5d4518e37ddc600c
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2022-02-07 18:57:53 -0600

    Sidebar Component

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sideBar">
+      <h1>CSS GRADIENT GENERATOR</h1>
+      <footer>
+        <p>Gradient Generator for Linear and Radial Css Color Gradients</p>
+        <p>
+          © 2021 CSS-GRADIENT.COM <br></br> Terms & Privacy - Contact
+        </p>
+      </footer>
+    </div>
+  );
+}
